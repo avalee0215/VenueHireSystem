a0196cec2ae9cc8232d72119769e5bba79116112
Chaeeun <clee482@aucklanduni.ac.nz>
2024-03-27 01:55:44 +1300

    If the typed venueCode is used previously, does not create the new venue, and print out the specific message. Use boolean and for loop for this.

--- src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
+++ src/main/java/nz/ac/auckland/se281/VenueHireSystem.java
@@ -5,14 +5,16 @@ import nz.ac.auckland.se281.Types.CateringType;
 import nz.ac.auckland.se281.Types.FloralType;
 
 public class VenueHireSystem {
-  ArrayList<venuesave> venuelist = new ArrayList<>(); //New Arraylist to save venue's information. Change <String> to <venuesave> so we can use the constructor
-  public VenueHireSystem() {
+  ArrayList<venuesave> venuelist =
+      new ArrayList<>(); // New Arraylist to save venue's information. Change <String> to
 
-  }
+  // <venuesave> so we can use the constructor
+
+  public VenueHireSystem() {}
 
   public void printVenues() {
     // TODO implement this method
-    
+
     ArrayList<String> venuenumber = new ArrayList<>(); // Create the arraylist that save one to nine
     venuenumber.add("one");
     venuenumber.add("two");
@@ -24,78 +26,97 @@ public class VenueHireSystem {
     venuenumber.add("eight");
     venuenumber.add("nine");
 
-    if (venuelist.size()==0) // when there is no venue, // change i to venuelist.size
-    { MessageCli.NO_VENUES.printMessage(); // Use MessageCli to print
-    }
-    else if (0<venuelist.size() && venuelist.size()<10){ // change i to venuelist.size
-      if (venuelist.size()==1) // change i to venuelist.size
+    if (venuelist.size() == 0) // when there is no venue, // change i to venuelist.size
+    {
+      MessageCli.NO_VENUES.printMessage(); // Use MessageCli to print
+    } else if (0 < venuelist.size() && venuelist.size() < 10) { // change i to venuelist.size
+      if (venuelist.size() == 1) // change i to venuelist.size
       {
-        MessageCli.NUMBER_VENUES.printMessage("is", venuenumber.get(0), ""); //print the message stating how many venues are there. In this case, one.
+        MessageCli.NUMBER_VENUES.printMessage(
+            "is",
+            venuenumber.get(0),
+            ""); // print the message stating how many venues are there. In this case, one.
 
+      } else {
+        int venuelistsize = venuelist.size() - 1;
+        MessageCli.NUMBER_VENUES.printMessage(
+            "are",
+            venuenumber.get(venuelistsize),
+            "s"); // print the message stating how many venues are there
       }
-      else {
-        int venuelistsize = venuelist.size()-1;
-        MessageCli.NUMBER_VENUES.printMessage("are", venuenumber.get(venuelistsize), "s"); //print the message stating how many venues are there
+      for (int j = 0; j < venuelist.size(); j++) {
+        venuesave venue = venuelist.get(j); // change code to use modified venuelist.
+        MessageCli.VENUE_ENTRY.printMessage(
+            venue.getvenuename(), venue.getvenueCode(), venue.getcapacity(), venue.gethirefee());
       }
-      for (int j = 0; j<venuelist.size(); j++){
-        venuesave venue = venuelist.get(j); //change code to use modified venuelist.
-        MessageCli.VENUE_ENTRY.printMessage(venue.getvenuename(),venue.getvenueCode(),venue.getcapacity(),venue.gethirefee());
-      }
-    }
-    else // if created venues are 10 or more than 10
+    } else // if created venues are 10 or more than 10
     {
       int venuelistsize = venuelist.size();
-      MessageCli.NUMBER_VENUES.printMessage("are",String.valueOf(venuelistsize),"s"); //print the message stating how many venues are there
-      for (int j = 0; j<venuelist.size(); j++){
+      MessageCli.NUMBER_VENUES.printMessage(
+          "are",
+          String.valueOf(venuelistsize),
+          "s"); // print the message stating how many venues are there
+      for (int j = 0; j < venuelist.size(); j++) {
         venuesave venue = venuelist.get(j);
-        MessageCli.VENUE_ENTRY.printMessage(venue.getvenuename(),venue.getvenueCode(),venue.getcapacity(),venue.gethirefee());
+        MessageCli.VENUE_ENTRY.printMessage(
+            venue.getvenuename(), venue.getvenueCode(), venue.getcapacity(), venue.gethirefee());
       }
     }
-
-    
-    
-  }  
-    
-  
+  }
 
   public void createVenue(
       String venueName, String venueCode, String capacityInput, String hireFeeInput) {
     // TODO implement this method
-    int capacitytonumber = Integer.parseInt(capacityInput);
-    boolean hireFeeinteger = true;
+    int capacitytonumber = Integer.parseInt(capacityInput); // convert string to integer
+    boolean hireFeeinteger =
+        true; // check that the hireFeeinput is integer or not initially (create it to use in the if
+    // else statement)
     hireFeeinteger = hireFeeInput.matches("\\d+");
+    boolean repeatedvenueCode = false;
+    int repeatedvenuenumber = 0;
 
-    
+    // Check if the venue code is already used
+    for (venuesave item : venuelist) {
+      if (item.getvenueCode().equals(venueCode)) {
+        repeatedvenueCode = true;
+        repeatedvenuenumber = venuelist.indexOf(item);
+        break;
+      }
+    }
 
     if (venueName.trim().isEmpty()) // if the trimmed string is empty, print the message
     {
       MessageCli.VENUE_NOT_CREATED_EMPTY_NAME.printMessage();
-    }
-    else if (capacitytonumber <= 0){
-      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity"," positive"); 
-    }
-    
-    else if (hireFeeinteger != true){
-      try{
+    } else if (capacitytonumber <= 0) {
+      MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("capacity", " positive");
+    } else if (hireFeeinteger != true) {
+      try {
         int hirefeetonumber = Integer.parseInt(hireFeeInput);
-        if (hirefeetonumber <=0)
-        {
-          MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee"," positive"); 
+        if (hirefeetonumber <= 0) {
+          MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", " positive");
         }
-      }
-      catch(NumberFormatException e){
+      } catch (NumberFormatException e) {
         MessageCli.VENUE_NOT_CREATED_INVALID_NUMBER.printMessage("hire fee", "");
       }
+    } else if (repeatedvenueCode) // If the code already used,
+    {
+      String usedvenue =
+          venuelist
+              .get(repeatedvenuenumber)
+              .getvenuename(); // to find out the name of the venue that already used the code
+      MessageCli.VENUE_NOT_CREATED_CODE_EXISTS.printMessage(venueCode, usedvenue);
+    } else {
+
+      MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(
+          venueName, venueCode); // Print out Successfully created venue by using MessageCli
+      venuelist.add(
+          new venuesave(
+              venueName,
+              venueCode,
+              capacityInput,
+              hireFeeInput)); // add venue's information in the venuelist
     }
-    else
-  {
-
-    MessageCli.VENUE_SUCCESSFULLY_CREATED.printMessage(venueName,venueCode); // Print out Successfully created venue by using MessageCli
-    venuelist.add(new venuesave(venueName, venueCode, capacityInput, hireFeeInput)); // add venue's information in the venuelist
   }
-    
-  }
-
 
   public void setSystemDate(String dateInput) {
     // TODO implement this method
--- src/main/java/nz/ac/auckland/se281/venuesave.java
+++ src/main/java/nz/ac/auckland/se281/venuesave.java
@@ -2,34 +2,30 @@ package nz.ac.auckland.se281;
 
 public class venuesave {
   String venueName;
-  String venueCode; 
+  String venueCode;
   String capacityInput;
   String hireFeeInput;
 
-  public venuesave(String venueName, String venueCode, String capacityInput, String hirefeeInput)
-  {
+  public venuesave(String venueName, String venueCode, String capacityInput, String hirefeeInput) {
     this.venueName = venueName;
     this.venueCode = venueCode;
     this.capacityInput = capacityInput;
     this.hireFeeInput = hirefeeInput;
   }
 
-  public String getvenuename(){
+  public String getvenuename() {
     return venueName;
   }
 
-  public String getvenueCode(){
+  public String getvenueCode() {
     return venueCode;
   }
 
-  public String getcapacity(){
+  public String getcapacity() {
     return capacityInput;
   }
 
-  public String gethirefee(){
+  public String gethirefee() {
     return hireFeeInput;
   }
-
 }
-
-
