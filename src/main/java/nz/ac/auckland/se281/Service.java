edf442ccbd771057a66b745eae5650b56296a745
avalee0215 <161207991+avalee0215@users.noreply.github.com>
2024-04-19 05:11:12 +1200

    Finish the Service class (abstract class). Basically, the abstract class has two variables that indicate the total cost and the type of the service.

--- src/main/java/nz/ac/auckland/se281/Service.java
+++ src/main/java/nz/ac/auckland/se281/Service.java
@@ -1,3 +1,25 @@
 package nz.ac.auckland.se281;
 
-public abstract class Service {}
+public abstract class Service {
+  String
+      name; // name is a type of the service. For example, when I use B in a catering service, the
+            // name will be 'Breakfast'
+  String totalCost; // total cost is the a cost of that specific type of the service.
+
+  public Service() {
+    this.totalCost = "0"; // Initially, totalCost is 0
+  }
+
+  public String getTotalCost() {
+    return totalCost;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public abstract void totalCostService(
+      String reference,
+      BookingSave
+          booking); // abstract class to use for the @Override. This will state the new totalcost
+}
