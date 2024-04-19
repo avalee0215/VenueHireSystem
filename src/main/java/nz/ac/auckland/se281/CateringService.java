97559f38937667aa36e65461bda9a7d1eca67266
avalee0215 <161207991+avalee0215@users.noreply.github.com>
2024-04-19 14:01:21 +1200

    Follow the VNAME rule in CateringService class. Change costPerperson to costPerPerson, bookingCapacity to venueCapacity

--- src/main/java/nz/ac/auckland/se281/CateringService.java
+++ src/main/java/nz/ac/auckland/se281/CateringService.java
@@ -3,25 +3,25 @@ package nz.ac.auckland.se281;
 import nz.ac.auckland.se281.Types.CateringType;
 
 public class CateringService extends Service {
-  int costPerperson; // as the catering service provides the cost per person only, new variable is
+  int costPerPerson; // as the catering service provides the cost per person only, new variable is
   // needed
   private int
-      bookingCapacity; // find the venue capacity of the given reference number. It won't be change
+      venueCapacity; // find the venue capacity of the given reference number. It won't be change
 
   public CateringService(CateringType cateringType) {
     super(); // bring the values from the Service class, so I can save the name of the service and
     // the total cost
     this.name = cateringType.getName();
-    this.costPerperson = cateringType.getCostPerPerson();
+    this.costPerPerson = cateringType.getCostPerPerson();
   }
 
   @Override
   public void totalCostService(String reference, Booking booking) {
-    bookingCapacity = Integer.parseInt(booking.getBookingCapacity());
+    venueCapacity = Integer.parseInt(booking.getBookingCapacity());
     this.totalCost =
         String.valueOf(
-            costPerperson
-                * bookingCapacity); // total cost of the catering service is the capacity * cost per
+            costPerPerson
+                * venueCapacity); // total cost of the catering service is the capacity * cost per
     // person
   }
 }
