a310dc39151338f7db0244aca611a56fc39eb336
avalee0215 <161207991+avalee0215@users.noreply.github.com>
2024-04-19 12:32:03 +1200

    Modify MusicService sub class so I can use it in the VenueHireSystem addServiceMusic class. It will give the total cost of the music service

--- src/main/java/nz/ac/auckland/se281/MusicService.java
+++ src/main/java/nz/ac/auckland/se281/MusicService.java
@@ -1,3 +1,13 @@
 package nz.ac.auckland.se281;
 
-public class MusicService extends Service {}
+public class MusicService extends Service {
+  private String cost =
+      "500"; // Because Music has only one, change the total cost to 500. Otherwise, it is 0.
+
+  public MusicService() {
+    this.totalCost = "" + cost;
+  }
+
+  @Override
+  public void totalCostService(String bookingReference, BookingSave booking) {}
+}
